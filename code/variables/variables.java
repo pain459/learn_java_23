dcacd61587de33f8e553287581bcc6eb04bb3afc
Ravi <ravi.ameer@gmail.com>
2023-01-20 21:40:43 +0530

    c1

---
+++ code/variables/variables.java
@@ -0,0 +1,9 @@
+class variables {
+    public static void main(String[] args) {
+        int a = 20;
+        float b = 2.5f;
+        double c = 200;
+        System.out.println(a + b);
+        System.out.println(c);
+    }
+}
