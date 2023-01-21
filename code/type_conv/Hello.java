6cc0b9d4d78ab52e7eec3b4e6965c67798d9cb26
Ravi <ravi.ameer@gmail.com>
2023-01-21 07:43:09 +0530

    upto type conversion

---
+++ code/type_conv/Hello.java
@@ -0,0 +1,17 @@
+class Hello{
+    public static void main(String[] args) {
+        // byte b = 127;
+        int a = 257;
+        byte k = (byte) a;
+
+        float f = 5.6f;
+        int t = (int) f;
+
+        byte c = 10;
+        byte d = 30;
+        int result = c * d;
+
+        System.out.println(t);
+        System.out.println(result);
+    }
+}
---
+++ code/variables/HelloVariable1.java
@@ -0,0 +1,20 @@
+public class HelloVariable1 {
+    public static void main(String args[]){
+        int a = 10;
+        float b = 2.5f;
+        char c = 'x';
+        boolean d = false;
+        long e = 58895l;
+        System.out.println(a * b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        int num1 = 0x7E;
+        System.out.println(num1);
+        double num2 = 100_000_000;
+        System.out.println(num2);
+        double num3 = 12e10;
+        System.out.println(num3);
+    }
+}
+
